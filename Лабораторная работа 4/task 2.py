5d8f25e265eeea1bb8133f8acd1a314e8c5b8d32
katkzzz <117278494+katkzzz@users.noreply.github.com>
2024-11-17 17:27:26 +0600

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task 2.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME, newline='', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)  # TODO считать содержимое csv файла
+
+        data = [row for row in reader]
+
+
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, indent=4, ensure_ascii=False)  # TODO Сериализовать в файл с отступами равными 4
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
